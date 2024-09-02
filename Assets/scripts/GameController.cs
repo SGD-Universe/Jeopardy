c3cc7d5eea7fb749f25cd4f7d5918e76a859416a
Marcos Perez Cruz <mperezc2@email.cpcc.edu>
2024-09-02 15:41:31 -0400

    I added the save program

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class GameSceneController : MonoBehaviour
@@ -8,24 +6,50 @@ public class GameSceneController : MonoBehaviour
     public GameObject teamPanelPrefab;
     public Transform teamPanelParent; // This should be the TeamPanelContainer
 
-    private List<TeamPanelController> teamPanelControllers;
+    public List<TeamPanelController> teamPanelControllers;
 
     private void Start()
     {
         Debug.Log("GameSceneController Start");
         teamPanelControllers = new List<TeamPanelController>();
+
+        if (JeperdyGameController.Instance == null)
+        {
+            Debug.LogError("JeperdyGameController instance is not set. Make sure it's initialized in the previous scene.");
+            return;
+        }
+
         CreateTeamPanels();
     }
 
     private void CreateTeamPanels()
     {
         Debug.Log("CreateTeamPanels called");
+
         var teams = JeperdyGameController.Instance.GetTeams();
+        if (teams == null || teams.Count == 0)
+        {
+            Debug.LogError("No teams found. Ensure SetupTeams() was called before transitioning to the game scene.");
+            return;
+        }
+
         Debug.Log("Number of teams: " + teams.Count);
 
         for (int i = 0; i < teams.Count; i++)
         {
             Debug.Log("Creating panel for team: " + teams[i].teamName);
+
+            if (teamPanelPrefab == null)
+            {
+                Debug.LogError("teamPanelPrefab is not set in the inspector!");
+                continue;
+            }
+            if (teamPanelParent == null)
+            {
+                Debug.LogError("teamPanelParent is not set in the inspector!");
+                continue;
+            }
+
             GameObject panel = Instantiate(teamPanelPrefab, teamPanelParent);
             if (panel == null)
             {
