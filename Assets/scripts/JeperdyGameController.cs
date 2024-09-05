3944d2351babc3d80d223ca83e06793ac43d5771
HeroOfFlames <89414423+HeroOfFlames@users.noreply.github.com>
2024-09-05 12:37:02 -0400

    Revert "Save"

--- Assets/scripts/JeperdyGameController.cs
+++ Assets/scripts/JeperdyGameController.cs
@@ -22,7 +22,7 @@ public class JeperdyGameController : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-           DontDestroyOnLoad(gameObject);
+           /* DontDestroyOnLoad(gameObject);*/
         }
         else
         {
