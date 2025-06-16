182c3d10b0a7943b8d195766373ecebeb965740b
OrdinaryRock <masonfilbrandt@gmail.com>
2025-06-15 23:19:25 -0400

    Added several more features
    - Warning flashes and messages
    - Title input
    - File Saving

--- Assets/Scripts/ExperimentalCreationController.cs
+++ Assets/Scripts/ExperimentalCreationController.cs
@@ -1,39 +1,204 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ExperimentalCreationController : MonoBehaviour
 {
     [SerializeField] private GameObject panelTemplate;
     [SerializeField] private Vector2 panelSpacing = new Vector2(4, 3);
-    private Vector2 panelCount = new Vector2(1, 1);
+    [SerializeField] private TMP_InputField titleInput;
+    [SerializeField] private float titleInputColorLerpFactor = 0f;
+    [SerializeField] private CanvasGroup warningPanel;
+    [SerializeField] private TMP_Text warningText;
+    [SerializeField] private float warningPanelAlphaLerpFactor = 0f;
+    [SerializeField] private Color errorColor;
+
+    private Color titleInputOriginalColor;
+
+    private Vector2 panelCount = new Vector2(6, 6);
+    private List<List<MonitorPlane>> panelsBoard = new List<List<MonitorPlane>>();
+
+    private Animator animator;
+    
+    [System.Serializable]
+    private struct PanelData {
+        public bool isCategory;
+        public string primaryText;
+        public string secondaryText;
+    }
+
+    [System.Serializable]
+    private struct ColumnData
+    {
+        public List<PanelData> panels;
+    }
+
+    private struct BoardData
+    {
+        public List<ColumnData> columns;
+    }
+
+    private BoardData boardData;
 
     // Start is called before the first frame update
     void Start()
     {
-        // RectTransform panelRectTransform = panelTemplate.GetComponent<RectTransform>();
-        // float panelWidth = panelRectTransform.localScale.x * panelRectTransform.rect.size.x;
-        // float panelHeight = panelRectTransform.localScale.y * panelRectTransform.rect.size.y;
-        // Debug.Log(panelWidth + " " + panelHeight);
         for(int c  = 0; c < panelCount.x; c ++)
         {
+            List<MonitorPlane> panelsColumn = new List<MonitorPlane>();
             for(int r  = 0; r < panelCount.y; r ++)
             {
-                // float spawnX = (c - panelCount.x/2) * panelWidth + (c - panelCount.x/2) * panelSpacing.x;
-                // float spawnY = (r - panelCount.y/2) * panelHeight + (r - panelCount.y/2) * panelSpacing.y;
-                float spawnX = (c + 0.5f - panelCount.x / 2f) * panelTemplate.transform.localScale.x * panelSpacing.x;
-                float spawnY = (r + 0.5f - panelCount.y / 2f) * panelTemplate.transform.localScale.y * panelSpacing.y;
+                
+                // float spawnX = (c + 0.5f - panelCount.x / 2f) * panelTemplate.transform.localScale.x * panelSpacing.x;
+                // float spawnY = (r + 0.5f - panelCount.y / 2f) * panelTemplate.transform.localScale.y * panelSpacing.y;
+                // Spawns from top-to-bottom, left-to-right
+                float spawnX = ((c + 0.5f) - (panelCount.x / 2f)) * panelTemplate.transform.localScale.x * panelSpacing.x;
+                float spawnY = ((panelCount.y / 2f) - (r + 0.5f)) * panelTemplate.transform.localScale.y * panelSpacing.y;
                 Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0f);
+                
                 GameObject panelClone = Instantiate(panelTemplate, transform);
                 panelClone.transform.localPosition = spawnPosition;
+                
+                MonitorPlane monitorPlane = panelClone.GetComponent<MonitorPlane>();
+                if(r == 0) monitorPlane.SetPanelType(MonitorPlane.Type.Category);
+                else monitorPlane.SetPanelType(MonitorPlane.Type.Question);
+                panelsColumn.Add(monitorPlane);
             }
+            panelsBoard.Add(panelsColumn);
         }
         panelTemplate.SetActive(false);
+
+        titleInputOriginalColor = titleInput.colors.normalColor;
+
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.F1)) SaveBoardData();
+        if(Input.GetKeyDown(KeyCode.F5)) FillBoardWithDummyData();
+        if(Input.GetKeyDown(KeyCode.F9)) EraseBoardData();
+
+        UnityEngine.UI.ColorBlock titleInputColorBlockClone = titleInput.colors;
+        titleInputColorBlockClone.normalColor = Color.Lerp(titleInputOriginalColor, errorColor, titleInputColorLerpFactor);
+        titleInput.colors = titleInputColorBlockClone;
+
+        warningPanel.alpha = warningPanelAlphaLerpFactor;
+    }
+
+    private void SaveBoardData()
+    {
+        if(VerifyBoardData())
+        {
+            boardData = new BoardData();
+            boardData.columns = new List<ColumnData>();
+            foreach(List<MonitorPlane> panelsColumn in panelsBoard)
+            {
+                ColumnData currentColumnData = new ColumnData();
+                currentColumnData.panels = new List<PanelData>();
+                foreach(MonitorPlane panel in panelsColumn)
+                {
+                    string string1 = panel.GetPrimaryInputString();
+                    string string2 = panel.GetSecondaryInputString();
+
+                    PanelData currentPanelData = new PanelData();
+                    currentPanelData.primaryText = string1;
+                    if(panel.GetPanelType() == MonitorPlane.Type.Category) currentPanelData.isCategory = true;
+                    else currentPanelData.secondaryText = string2;
+                    currentColumnData.panels.Add(currentPanelData);
+                }
+                boardData.columns.Add(currentColumnData);
+            }
+            string json = JsonUtility.ToJson(boardData, true);
+            string fileName = titleInput.text.Trim();
+            char[] invalid = Path.GetInvalidFileNameChars();
+            if(string.IsNullOrWhiteSpace(fileName))
+            {
+                animator.Play("CreationScreenTitleError", 0, 0f);
+                animator.Play("CreationScreenWarningFlash", 1, 0f);
+                warningText.text = "Please give your quiz a title";
+                return;
+            }
+            foreach(char c in fileName)
+            {
+                if(invalid.Contains(c))
+                {
+                    animator.Play("CreationScreenTitleError", 0, 0f);
+                    animator.Play("CreationScreenWarningFlash", 1, 0f);
+                    warningText.text = "Quiz title has invalid characters. Please give it a different name.";
+                    return;
+                }
+            }
+            string filePath = Application.persistentDataPath + "/" + fileName + ".json";
+            File.WriteAllText(filePath, json);
+            Process.Start("explorer.exe", "/select,\"" + Path.GetFullPath(filePath) + "\"");
+        }
+    }
+
+    private bool VerifyBoardData()
+    {
+        bool dataIsValid = true;
+        foreach(List<MonitorPlane> panelsColumn in panelsBoard)
+        {
+            foreach(MonitorPlane panel in panelsColumn)
+            {
+                string string1 = panel.GetPrimaryInputString();
+                string string2 = panel.GetSecondaryInputString();
+                if(string.IsNullOrEmpty(string1))
+                {
+                    panel.FlashError();
+                    dataIsValid = false;
+                }
+                else if(panel.GetPanelType() == MonitorPlane.Type.Question && string.IsNullOrEmpty(string2))
+                {
+                    panel.FlashError();
+                    dataIsValid = false;
+                }
+            }
+        }
+        if(dataIsValid == false)
+        {
+            animator.Play("CreationScreenWarningFlash", 1, 0f);
+            warningText.text = "Please fill out all the panels";
+        }
+        return dataIsValid;
+    }
+
+    private void FillBoardWithDummyData()
+    {
+        foreach(List<MonitorPlane> panelsColumn in panelsBoard)
+        {
+            foreach(MonitorPlane panel in panelsColumn)
+            {
+                if(panel.GetPanelType() == MonitorPlane.Type.Category)
+                {
+                    panel.SetPrimaryInputString("SHOES");
+                }
+                else if(panel.GetPanelType() == MonitorPlane.Type.Question)
+                {
+                    panel.SetPrimaryInputString("These shoes are named after Florida's iconic reptile");
+                    panel.SetSecondaryInputString("What are Crocs?");
+                }
+            }
+        }
+    }
+
+    private void EraseBoardData()
+    {
+        foreach(List<MonitorPlane> panelsColumn in panelsBoard)
+        {
+            foreach(MonitorPlane panel in panelsColumn)
+            {
+                panel.SetPrimaryInputString("");
+                panel.SetSecondaryInputString("");
+            }
+        }
     }
 }
--- Assets/Scripts/MonitorPlane.cs
+++ Assets/Scripts/MonitorPlane.cs
@@ -1,89 +1,218 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class MonitorPlane : MonoBehaviour
 {
-    [SerializeField] private float animationLerpFactor;
-    private float animationLerpFactorAdjusted;
-    private int animationDirection = 1;
+    // Used to animate the position of the panel to the center of the screen
+    // (Grow/shrink animations are handled with an actual animator component)
+    [SerializeField] private float positionLerpFactor;
+    [SerializeField] private float colorLerpFactor;
+    [SerializeField] private Color errorColor;
+    [SerializeField] private TMP_InputField primaryInputField;
+    [SerializeField] private TMP_InputField secondaryInputField;
+    [SerializeField] private TMP_Text dividerGraphic;
+    [SerializeField] private TMP_FontAsset categoryFont;
+    [SerializeField] private float inputFieldsSpacing = 0.5f;
+
+    private TMP_Text placeholderText;
+    private string primaryInputString = "";
+    private string secondaryInputString = "";
+
+    public enum Type
+    {
+        Category,
+        Question
+    }
+
+    private Type type;
 
     private Animator animator;
-    
-    private Vector3 baseScale;
-    private Vector3 originalScale;
-    private Vector3 hoverScale;
-    private Vector3 fullscreenScale;
 
+    private Material material;
+    private Color originalColor;
     private Vector3 originalPosition;
     private Vector3 fullscreenPosition;
+
+    private bool isFullscreen = false;
+    private bool isHovered = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        placeholderText = primaryInputField.placeholder as TMP_Text;
+
         animator = GetComponent<Animator>();
         animator.enabled = true;
+        animator.SetLayerWeight(1, 1f);
+        animator.SetLayerWeight(2, 1f);
 
-        originalScale = fullscreenScale = transform.localScale;
-        originalPosition = fullscreenPosition = transform.localPosition;
+        originalPosition = transform.localPosition;
 
-        fullscreenScale = originalScale * 6.2f;
-        fullscreenPosition = Vector3.zero;
+        fullscreenPosition = new Vector3(0, 0, -1);
 
-
-
+        material = GetComponent<MeshRenderer>().material;
+        originalColor = material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        if(animationDirection == 1) animationLerpFactorAdjusted = animationLerpFactor;
-        else animationLerpFactorAdjusted = 1 - animationLerpFactor;
+        Vector3 calculatedPosition = Vector3.Lerp(originalPosition, fullscreenPosition, positionLerpFactor);
+        if(isHovered || isFullscreen) calculatedPosition.z = -1f;
+        else calculatedPosition.z = 0f;
+        transform.localPosition = calculatedPosition;
 
-        transform.localPosition = Vector3.Lerp(originalPosition, fullscreenPosition, animationLerpFactorAdjusted);
-        transform.localScale = Vector3.Lerp(originalScale, fullscreenScale, animationLerpFactorAdjusted);
+        material.color = Color.Lerp(originalColor, errorColor, colorLerpFactor);
 
-        */
+        if(isFullscreen && type == Type.Question)
+        {
+            primaryInputField.transform.localPosition = new Vector3(-0.1f, inputFieldsSpacing, 0f);
+            secondaryInputField.transform.localPosition = new Vector3(-0.1f, -inputFieldsSpacing, 0f);
+        }
+        else primaryInputField.transform.localPosition = Vector3.forward * -0.1f;
+
+        primaryInputString = primaryInputField.text;
+        secondaryInputString = secondaryInputField.text;
+
         if(Input.GetKeyDown(KeyCode.Escape)) OnEscapeDown();
     }
 
+    private void OnMouseOver()
+    {
+        if(!isHovered && !isFullscreen)
+        {
+            animator.CrossFadeInFixedTime("MonitorPlaneHover", 0.1f);
+            isHovered = true;
+        }
+
+    }
+
+    /*
     private void OnMouseEnter()
     {
-        
+        if(!isFullscreen)
+            animator.CrossFadeInFixedTime("MonitorPlaneHover", 0.1f);
     }
+    */
 
     private void OnMouseExit()
     {
-        
+        if(!isFullscreen)
+        {
+            animator.CrossFadeInFixedTime("MonitorPlaneUnhover", 0.1f);
+            isHovered = false;
+        }
+
     }
 
     private void OnMouseDown()
     {
-        animator.Play("MonitorPlaneFullscreen");
-        /*
-        animationDirection = 1;
-        animationLerpFactor = 0f;
-        animator.enabled = true;
-        animator.Play(0);
+        if(!isFullscreen)
+        {
+            animator.Play("MonitorPlaneFullscreen");
+            animator.Play("MonitorPlaneCenter");
 
-        // targetScale = originalScale * 6.2f;
-        // targetPosition = Vector3.zero;
-        */
+            isFullscreen = true;
+            isHovered = false;
+
+            primaryInputField.interactable = true;
+            if(type == Type.Question)
+            {
+                primaryInputField.pointSize = 12f;
+                secondaryInputField.gameObject.SetActive(true);
+                secondaryInputField.interactable = true;
+                dividerGraphic.gameObject.SetActive(true);
+            }
+
+
+        }
     }
 
     private void OnEscapeDown()
     {
-        animator.Play("MonitorPlaneMinimize");
+        if(isFullscreen)
+        {
+            animator.Play("MonitorPlaneMinimize");
+            animator.Play("MonitorPlaneUncenter");
 
-        /*
-        animationDirection = -1;
-        animationLerpFactor = 0f;
-        animator.Play(0);
+            isFullscreen = false;
 
-        // targetScale = originalScale;
-        // targetPosition = originalPosition;
-        */
+            primaryInputField.interactable = false;
+            // primaryInputField.DeactivateInputField(true);
+
+            if(type == Type.Question)
+            {
+                primaryInputField.pointSize = 18f;
+                secondaryInputField.gameObject.SetActive(false);
+                secondaryInputField.interactable = false;
+                secondaryInputField.DeactivateInputField();
+                dividerGraphic.gameObject.SetActive(false);
+            }
+
+
+        }
+    }
+
+    public void SetPanelType(Type type)
+    {
+        this.type = type;
+        UpdatePlaceholderText();
+    }
+
+    public Type GetPanelType()
+    {
+        return this.type;
+    }
+
+    private void UpdatePlaceholderText()
+    {
+        if(primaryInputField == null) Debug.LogError("You forgot to assign a reference to the InputField in the MonitorPlane template, silly");
+        else
+        {
+            if(placeholderText == null) placeholderText = primaryInputField.placeholder as TMP_Text;
+            if(type == Type.Category)
+            {
+                primaryInputField.pointSize = 40f;
+                RectTransform _rect = primaryInputField.gameObject.GetComponent<RectTransform>();
+                Vector2 size = _rect.sizeDelta;
+                size.y = 164f;
+                _rect.sizeDelta = size;
+                placeholderText.text = "<b>ENTER CATEGORY...</b>";
+                // placeholderText.fontSize = 30f;
+                placeholderText.font = categoryFont;
+            }
+            else if(type == Type.Question) placeholderText.text = "Enter Question...";
+        }
+    }
+
+    public string GetPrimaryInputString()
+    {
+        return primaryInputString;
+    }
+
+    public void SetPrimaryInputString(string newText)
+    {
+        primaryInputField.text = newText;
+        primaryInputString = newText;
+    }
+    
+    public string GetSecondaryInputString()
+    {
+        return secondaryInputString;
+    }
+
+    public void SetSecondaryInputString(string newText)
+    {
+        secondaryInputField.text = newText;
+        secondaryInputString = newText;
+    }
+
+    public void FlashError()
+    {
+        animator.Play("MonitorPlaneError", 2, 0f);
     }
 }
