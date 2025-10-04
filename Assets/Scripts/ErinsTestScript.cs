29c26423be7aadcb811b1a71bf57c92ebec54732
Luckyee <Artbyluckyee@gmail.com>
2025-10-04 14:22:23 -0400

    Test files added
    For America, test files, do not merge

---
+++ Assets/Scripts/ErinsTestScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErinsTestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        // Test to show America how to send in changes!
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
