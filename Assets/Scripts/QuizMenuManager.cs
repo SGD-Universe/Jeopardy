5bce641856e9e7c3846bb19d865ae05a47122473
PatB-git <pbartho1@email.cpcc.edu>
2026-02-11 16:02:29 -0500

    Add files via upload
    Fix Main Menu Buttons

--- Assets/Scripts/QuizMenuManager.cs
+++ Assets/Scripts/QuizMenuManager.cs
@@ -11,9 +11,9 @@ public class QuizMenuManager : MonoBehaviour
     public Button importQuizButton;
     public Button backButton;
 
-    public string createQuizSceneName = "test-creation-mode";
-    public string editQuizSceneName = "EditQuizScene";
-    public string mainMenuSceneName = "Main";
+    string createQuizSceneName = "test-creation-mode";
+    string editQuizSceneName = "EditQuizScene";
+    string mainMenuSceneName = "Main";
 
     void Start()
     {
