186cb26c85e3cb1a1e3caa95fd1b6d339bece775
Don Albert Collins <16708946+TheDelta491@users.noreply.github.com>
2026-04-27 22:59:23 -0400

    Brief changes
    Saved Game Button Prefab
    -Created the prefab.
    
    SavedGameButton.cs
    -Started setting up the script.

--- Assets/Scripts/UI/Scenes/Main/World Space/Main Menu/Menu Buttons/SavedGameButton.cs
+++ Assets/Scripts/UI/Scenes/Main/World Space/Main Menu/Menu Buttons/SavedGameButton.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class SavedGameButton : MonoBehaviour
 {
+    [Header("Button")]
+    [SerializeField] private Button savedGameButton;
+
     [Header("Text Components")]
     [SerializeField] private TextMeshProUGUI quizNameText;
     [SerializeField] private TextMeshProUGUI teamScoreText;
+
+    void Awake()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        savedGameButton.onClick.AddListener(LoadSelectedGame);
+    }
+
+    void OnDisable()
+    {
+        savedGameButton.onClick.RemoveAllListeners();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +34,8 @@ public class SavedGameButton : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadSelectedGame()
     {
-        
+
     }
 }
