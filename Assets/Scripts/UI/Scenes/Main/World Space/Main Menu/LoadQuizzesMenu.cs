663ae1998f901419d463dfb9d7b360dbc40dd9a9
sophiemarten <p27dee@gmail.com>
2026-06-12 14:32:41 -0400

    working on scroll rect

--- Assets/Scripts/UI/Scenes/Main/World Space/Main Menu/LoadQuizzesMenu.cs
+++ Assets/Scripts/UI/Scenes/Main/World Space/Main Menu/LoadQuizzesMenu.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class LoadQuizzesMenu : MonoBehaviour
 {
+    public ScrollRect sR;
+    public RectTransform content;
+
     // Start is called before the first frame update
     void Start()
     {
